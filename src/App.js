751e5780b7af23d0c5eb4b15c14d49e6ad42ad2f
Orion <wilman3d@hotmail.com>
2023-05-18 20:27:22 -0500

    Conectado API

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './assets/css/componentes/card.css';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Page404 from './pages/Page404';
-import Header from './components';
+import Header from './components/Header';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
