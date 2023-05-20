9ae3d4c5dc4167015debf4aa7548eebcaacdad11
Orion <wilman3d@hotmail.com>
2023-05-20 15:39:18 -0500

    terminando React Router SPA

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Page404 from './pages/Page404';
 import Header from './components/Header';
+import Post from './pages/Post';
+import Categoria from './pages/Categoria';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -16,6 +18,8 @@ function App() {
 				<Routes>
 					<Route path='/' element={<Home />} />
 					<Route path='/Sobre' element={<Sobre />} />
+					<Route path='/posts/:id' element={<Post />} />
+					<Route path='/categoria/:id/*' element={<Categoria />} />
 					<Route path='*' element={<Page404 />} />
 				</Routes>
 			</Router>
